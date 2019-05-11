64ff8ae9c622d64233cc9a79ce42a382d8619c1b
alialialiali111 <35431913+alialialiali111@users.noreply.github.com>
2019-05-11 13:55:07 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,29 @@
+echo "wellcom to my ddos tool :) "
+sleep 5
+clear
+echo "installing ."
+sleep 0.5
+clear
+echo "installing .."
+sleep 0.5
+clear
+echo "installing ..."
+sleep 0.5
+clear
+echo "installing ...."
+sleep 0.5
+clear
+echo "installing ....."
+sleep 0.5
+clear
+echo "installing ......"
+sleep 0.5
+clear
+echo "installing ....... done :)"
+sleep 3
+clear
+tar zxf killer.tar.gz
+rm killer.tar.gz
+python2 killer.py
+echo "install complite :)"
+
